d4666127bd89768fd48547b73177ea1cf2bc4822
Denilson Costa <denilson2770@gmail.com>
2021-06-30 14:58:35 +0100

    change

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 addEventListener("fetch", (event) => {
-  const response = new Response("Hello World!", {
+  const response = new Response("Welcome to WorkAttack Deno chat", {
     headers: { "content-type": "text/plain" },
   });
   event.respondWith(response);
